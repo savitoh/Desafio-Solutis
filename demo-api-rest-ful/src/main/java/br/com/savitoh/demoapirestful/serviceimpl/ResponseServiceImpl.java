145b7496d28e3f1bc670429f3af4cc8ea0db1e46
savitoh <savioraires10@gmail.com>
2019-03-08 18:21:58 -0300

    Adição da anotação service em ResponseServiceImpl; Criação do controller

--- demo-api-rest-ful/src/main/java/br/com/savitoh/demoapirestful/serviceimpl/ResponseServiceImpl.java
+++ demo-api-rest-ful/src/main/java/br/com/savitoh/demoapirestful/serviceimpl/ResponseServiceImpl.java
@@ -4,7 +4,9 @@ import br.com.savitoh.demoapirestful.model.Response;
 import br.com.savitoh.demoapirestful.repository.ResponseRepository;
 import br.com.savitoh.demoapirestful.service.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ResponseServiceImpl implements ResponseService {
 
     private ResponseRepository responseRepository;
